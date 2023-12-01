273ac3b88a9623315c84e705ff87ef34bdbfb9a8
gamzemeryemkaya <gamzemeryemkaya@gmail.com>
2023-12-01 18:22:14 +0300

    Add project files.

---
+++ Core/UdemyCarBook.Domain/Entities/Blog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.Domain.Entities
+{
+    internal class Blog
+    {
+    }
+}
